93aa348f29f4a084041a7d98dbdd62c6bd3c3dee
Nol1fer <gluhov.m.n@gmail.com>
2022-07-30 09:57:25 +0500

    feat: prettier here

--- ar-cfg/src/index.ts
+++ ar-cfg/src/index.ts
@@ -1,8 +1,8 @@
-import "./style.scss";
-import { lul } from "./bruh";
+import './style.scss';
+import { lul } from './bruh';
 
 function name(a: number, b: string) {
-  return `${a} number, ${b} string, ${lul} import`;
+    return `${a} number, ${b} string, ${lul} import`;
 }
 
- console.log(name(3, 'bruh'));
+console.log(name(3, 'bruh'));
